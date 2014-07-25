a10104a4e2bcabe10f6d51941a7b317fa77d923d
Tim Murphy <tim@26tp.com>
2014-07-26 05:36:47 +1000

    Added AzureMagic.Storage.Tables.Specifications project.

---
+++ Projects/CommonAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("AzureMagic.Storage")]
+[assembly: AssemblyCopyright("Copyright © 2014 Tim Murphy")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
