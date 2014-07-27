160748abb66d5920cb0728cc826699dba8bf17b6
Tim Murphy <tim@26tp.com>
2014-07-27 11:32:28 +1000

    Refactoring.

--- Projects/AzureMagic.Storage.TableStorage/RestApi/TablesRestApi.cs
+++ Projects/AzureMagic.Storage.TableStorage/RestApi/TablesRestApi.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,12 @@ namespace AzureMagic.Storage.TableStorage.RestApi
     public class TablesRestApi
     {
         private readonly StorageAccount Account;
+
+        private enum KeyType
+        {
+            SharedKeyLite,
+            SharedKey
+        };
 
         public TablesRestApi(StorageAccount account)
         {
@@ -22,7 +31,7 @@ namespace AzureMagic.Storage.TableStorage.RestApi
 
             using (var client = new HttpClient())
             {
-               
+
                 var request = new HttpRequestMessage(HttpMethod.Get, new Uri(Account.TablesUri, "Tables"));
                 var headers = request.Headers;
 
@@ -41,9 +50,20 @@ namespace AzureMagic.Storage.TableStorage.RestApi
             }
         }
 
-        private static string GetAuthorizationHeader(HttpRequestMessage request, string account, string storageKey)
+        private static string GetAuthorizationHeader(HttpRequestMessage request, string account, string storageKey, KeyType keyType = KeyType.SharedKeyLite)
         {
             var sharedKey = Convert.FromBase64String(storageKey);
+            var resource = GetResource(request);
+            var stringToSign = GetStringToSign(request, account, keyType, resource);
+            var hasher = new HMACSHA256(sharedKey);
+            var signedSignature = Convert.ToBase64String(hasher.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
+            var authorizationHeader = string.Format("{0} {1}:{2}", keyType, account, signedSignature);
+
+            return authorizationHeader;
+        }
+
+        private static string GetResource(HttpRequestMessage request)
+        {
             var resource = request.RequestUri.PathAndQuery;
 
             if (resource.Contains("?"))
@@ -51,20 +71,39 @@ namespace AzureMagic.Storage.TableStorage.RestApi
                 resource = resource.Substring(0, resource.IndexOf("?", StringComparison.Ordinal));
             }
 
-            var stringToSign = string.Format("{0}\n/{1}{2}", request.Headers.Date.Value.ToString("R"), account, resource);
-//            var stringToSign = string.Format("{0}\n{1}\n{2}\n{3}\n/{4}/{5}",
-//method,
-//    client.DefaultRequestHeaders["Content-MD5"],
-//    request.Headers["Content-Type"],
-//    request.Headers["x-ms-date"],
-//    account,
-//    resource);
+            return resource;
+        }
 
-            var hasher = new HMACSHA256(sharedKey);
-            var signedSignature = Convert.ToBase64String(hasher.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
-            var authorizationHeader = string.Format("{0} {1}:{2}", "SharedKeyLite", account, signedSignature);
+        private static string GetStringToSign(HttpRequestMessage request, string account, KeyType keyType, string resource)
+        {
+            var headers = request.Headers;
 
-            return authorizationHeader;
+            switch (keyType)
+            {
+                case KeyType.SharedKeyLite:
+                    return string.Format("{0}\n/{1}{2}", headers.Date.GetValueOrDefault().ToString("R"), account, resource);
+
+                case KeyType.SharedKey:
+                    //return string.Format("{0}\n{1}\n{2}\n{3}\n/{4}/{5}",
+                    //    request.Method,
+                    //    TryGetHeaderValue(headers, "Content-MD5", ""),
+                    //    TryGetHeaderValue(headers, "Content-Type", ""),
+                    //    headers.Date.GetValueOrDefault().ToString("R"),
+                    //    account,
+                    //    resource);
+                    throw new NotImplementedException();
+
+                default:
+                    throw new NotImplementedException(string.Format("KeyType.{0} has not been implemented.", keyType));
+
+            }
+        }
+
+        private static string TryGetHeaderValue(HttpHeaders headers, string key, string defaultValue)
+        {
+            IEnumerable<string> values;
+
+            return headers.TryGetValues(key, out values) ? values.Single() : defaultValue;
         }
     }
 }
